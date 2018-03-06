177a210c9cb4c8a69706e1aa356c8fde2d1315a9
joelleishimwe <joelle.ishimwe@ocsbstudent.ca>
2018-03-06 10:15:33 -0500

    Initial commit

---
+++ main.lua
@@ -0,0 +1,7 @@
+-- Title: AnimatingImages
+-- Name: Joelle Ishimwe
+-- Course: ICS2O
+-- This program displays 3 animated images in diffrent direction, with some coloured text and a background.
+-----------------------------------------------------------------------------------------
+
+-- Your code here
